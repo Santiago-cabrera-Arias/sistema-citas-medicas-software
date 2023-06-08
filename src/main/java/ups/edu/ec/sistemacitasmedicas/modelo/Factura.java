23eddcb2fbeffdc0220495e54be482acbac290a4
Santiago <santiagocabrera354@gmail.com>
2023-06-08 00:55:10 -0500

    cambios en factura

--- src/main/java/ups/edu/ec/sistemacitasmedicas/modelo/Factura.java
+++ src/main/java/ups/edu/ec/sistemacitasmedicas/modelo/Factura.java
@@ -1,10 +1,12 @@
 package ups.edu.ec.sistemacitasmedicas.modelo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Factura {
 
+    @Id
     private int id;
     private String f;
 }
