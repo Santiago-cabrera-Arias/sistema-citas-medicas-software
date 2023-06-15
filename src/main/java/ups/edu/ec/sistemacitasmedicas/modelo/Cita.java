b525bb926a22aebfe76ccb5362db2662e3cf14d4
Paulina20lab <pazuero@est.ups.edu.ec>
2023-06-14 22:22:30 -0500

    Clases

---
+++ src/main/java/ups/edu/ec/sistemacitasmedicas/modelo/Cita.java
@@ -0,0 +1,82 @@
+package ups.edu.ec.sistemacitasmedicas.modelo;
+
+
+import jakarta.persistence.*;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Table(name = "medico")
+public class Cita {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JoinColumn(name = "cita_id")
+    private Long citaId;
+    @JoinColumn(name = "fecha")
+    private Date fecha;
+    @JoinColumn(name = "hora")
+    private Time hora;
+    @JoinColumn(name = "estado")
+    private Boolean estado;
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    public Cita(Long citaId, Date fecha, Time hora, Boolean estado, Medico medico) {
+        this.citaId = citaId;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = estado;
+        this.medico = medico;
+    }
+
+    public Cita(Long citaId) {
+        this.citaId = citaId;
+    }
+
+    public Cita() {
+
+    }
+
+    public Long getCitaId() {
+        return citaId;
+    }
+
+    public void setCitaId(Long citaId) {
+        this.citaId = citaId;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Time getHora() {
+        return hora;
+    }
+
+    public void setHora(Time hora) {
+        this.hora = hora;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+}
---
+++ src/main/java/ups/edu/ec/sistemacitasmedicas/modelo/Medico.java
@@ -0,0 +1,48 @@
+package ups.edu.ec.sistemacitasmedicas.modelo;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medico")
+public class Medico extends Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long MedicoId;
+    private String telenoConsultorio;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+    @ManyToOne
+    @JoinColumn(name = "especialidad_id")
+    private Especialidades especialidad;
+
+    public Medico(Long id, String cedula, String nombre, String apellido, String direccion, String telefono, String correo, String estado, String fechaNacimiento, String sexo, String tipo, boolean esCliente, boolean esMedico, boolean esEmpleado) {
+        super(Math.toIntExact(id), cedula, nombre, apellido, direccion, telefono, correo, estado, fechaNacimiento, sexo, tipo, esCliente, esMedico, esEmpleado);
+    }
+
+
+
+    public Medico() {
+        super();
+    }
+
+    public Long getMedicoId() {
+        return MedicoId;
+    }
+
+    public void setMedicoId(Long medicoId) {
+        MedicoId = medicoId;
+    }
+
+    public String getTelenoConsultorio(String correo) {
+        return telenoConsultorio;
+    }
+
+    public void setTelenoConsultorio(String telenoConsultorio) {
+        this.telenoConsultorio = telenoConsultorio;
+    }
+
+}
