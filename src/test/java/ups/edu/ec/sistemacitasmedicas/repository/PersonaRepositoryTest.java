ce87cd0f7cc8e6e883e25ab5dcbf5a939552ed15
Santiago <santiagocabrera354@gmail.com>
2023-06-17 19:37:47 -0500

    cambios

--- src/test/java/ups/edu/ec/sistemacitasmedicas/repository/PersonaRepositoryTest.java
+++ src/test/java/ups/edu/ec/sistemacitasmedicas/repository/PersonaRepositoryTest.java
@@ -14,20 +14,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PersonaRepositoryTest {
 
-    @Autowired
-    private PersonaRepositorio personaRepositorio;
-
-    @Test
-    void testGuardarPersona(){
-
-        Persona persona1 = new Persona("0100324534", "santiago", "cabrera", "Gualaceo", "0998087966", "ssc@gmail.com","activo",
-                "17-02-2002", "masculino", "Empleado");
-
-        Persona personaGuardado = personaRepositorio.save(persona1);
-
-        Assertions.assertNotNull(personaGuardado);
-
-    }
 
 
 }
